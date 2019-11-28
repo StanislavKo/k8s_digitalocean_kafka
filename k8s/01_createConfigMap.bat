315562fd17ac9923d4811c6842772d0840aa459e
StanislavKo <drstanislavkocherovets@gmail.com>
2019-11-28 17:20:35 +0300

    scaling up

--- k8s/01_createConfigMap.bat
+++ k8s/01_createConfigMap.bat
@@ -1,1 +1,10 @@
-kubectl --kubeconfig="do_config.yaml" create configmap config01 --from-file g:\temp\15\cerebro\k8s\kubectl_kafka\configmap 
+rem kubectl --kubeconfig="do_config.yaml" create configmap config01 --from-file g:\temp\15\cerebro\k8s\kubectl_kafka\configmap 
+DEL configJaas.yaml
+kubectl --kubeconfig="do_config.yaml" create configmap config-jaas --from-file g:\temp\15\cerebro\k8s\kubectl_kafka\configmapJaas\kafka_server_jaas.conf -o yaml --dry-run > configJaas.yaml
+kubectl --kubeconfig="do_config.yaml" apply -f g:\temp\15\cerebro\k8s\kubectl_kafka\configJaas.yaml
+
+DEL configNginx.yaml
+kubectl --kubeconfig="do_config.yaml" create configmap config-nginx --from-file g:\temp\15\cerebro\k8s\kubectl_kafka\configmapNginx -o yaml --dry-run > configNginx.yaml
+kubectl --kubeconfig="do_config.yaml" apply -f g:\temp\15\cerebro\k8s\kubectl_kafka\configNginx.yaml
+
+
--- k8s/02_createKafka.bat
+++ k8s/02_createKafka.bat
@@ -1,1 +1,2 @@
-kubectl --kubeconfig="do_config.yaml" create --save-config -f do_kafka.yaml
+rem kubectl --kubeconfig="do_config.yaml" create --save-config -f do_kafka.yaml
+kubectl --kubeconfig="do_config.yaml" apply -f do_kafka.yaml
